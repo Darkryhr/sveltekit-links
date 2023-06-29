8825b74032e29181fc934465ce47ff992e6e7865
Gabriel Lellouche <gabyk800@gmail.com>
2023-06-29 17:59:03 +0300

    custom usernames

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,8 +1,9 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app';
-import { getAuth } from 'firebase/auth';
+import { getAuth, onAuthStateChanged, type User } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
+import { writable } from 'svelte/store';
 
 const firebaseConfig = {
   apiKey: 'AIzaSyCUxjAY_bSs1H0kAq4n5EXAWUKfl2PbcLM',
@@ -17,3 +18,29 @@ const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
 export const auth = getAuth(app);
 export const storage = getStorage(app);
+
+function userStore() {
+  let unsubscribe: () => void;
+
+  if (!auth || !globalThis.window) {
+    console.warn('Auth is not initialized or not in browser');
+    const { subscribe } = writable<User | null>(null);
+    return {
+      subscribe,
+    };
+  }
+
+  const { subscribe } = writable(auth?.currentUser ?? null, set => {
+    unsubscribe = onAuthStateChanged(auth, user => {
+      set(user);
+    });
+
+    return () => unsubscribe();
+  });
+
+  return {
+    subscribe,
+  };
+}
+
+export const user = userStore();
